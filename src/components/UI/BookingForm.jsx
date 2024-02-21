0cda19cc624d78571391a4724041d2ea2444cf01
Khan <imamul.khan@openreach.co.uk>
2024-02-21 15:08:59 +0530

    Updated

---
+++ src/components/UI/BookingForm.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import "../../styles/booking-form.css";
+import { Form, FormGroup } from "reactstrap";
+
+const BookingForm = () => {
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <Form onSubmit={submitHandler}>
+      <FormGroup className="booking__form d-inline-block me-4 mb-4">
+        <input type="text" placeholder="First Name" />
+      </FormGroup>
+      <FormGroup className="booking__form d-inline-block ms-1 mb-4">
+        <input type="text" placeholder="Last Name" />
+      </FormGroup>
+
+      <FormGroup className="booking__form d-inline-block me-4 mb-4">
+        <input type="email" placeholder="Email" />
+      </FormGroup>
+      <FormGroup className="booking__form d-inline-block ms-1 mb-4">
+        <input type="number" placeholder="Phone Number" />
+      </FormGroup>
+
+      <FormGroup className="booking__form d-inline-block me-4 mb-4">
+        <input type="text" placeholder="Location" />
+      </FormGroup>
+      <FormGroup className="booking__form d-inline-block ms-1 mb-4">
+        <input type="text" placeholder="fav Branch" />
+      </FormGroup>
+
+      <FormGroup className="booking__form d-inline-block me-4 mb-4">
+        <select name="" id="">
+          <option value="1 person">1 Person</option>
+          <option value="2 person">2 Person</option>
+          <option value="3 person">3 Person</option>
+          <option value="4 person">4 Person</option>
+          <option value="5+ person">5+ Person</option>
+        </select>
+      </FormGroup>
+      <FormGroup className="booking__form d-inline-block ms-1 mb-4">
+        <select name="" id="">
+          <option value="1 luggage"> 5:00 AM to 5:45 AM</option>
+          <option value="2 luggage"> 5:00 AM to 6:45 AM</option>
+          <option value="3 luggage"> 7:00 AM to 7:45 AM</option>
+          <option value="4 luggage"> 8:00 AM to 8:45 AM</option>
+          <option value="5+ luggage"> 9:00 AM to 9:45 AM</option>
+        </select>
+      </FormGroup>
+
+      <FormGroup className="booking__form d-inline-block me-4 mb-4">
+        <input type="date" placeholder="Journey Date" />
+      </FormGroup>
+      <FormGroup className="booking__form d-inline-block ms-1 mb-4">
+        <input
+          type="time"
+          placeholder="Journey Time"
+          className="time__picker"
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <textarea
+          rows={5}
+          type="textarea"
+          className="textarea"
+          placeholder="Write"
+        ></textarea>
+      </FormGroup>
+    </Form>
+  );
+};
+
+export default BookingForm;
